cf4369f53e3f38d119a009908594c8726c6dd7f9
Kim, Hyeonseo <dodok8@gmail.com>
2025-07-27 16:09:22 +0900

    tutorial: Authorization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { user } from "./user";
+import { note } from "./note";
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+	// Apply the swagger plugin
+	.use(swagger())
+	.use(note)
+	.use(user)
+	.listen(3000);
